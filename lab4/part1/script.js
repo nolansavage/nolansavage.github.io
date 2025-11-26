fb47c37f8317baf4902b2032579e25d6b21863f8
nolansavage <nolan.savage@dcmail.ca>
2025-11-26 16:30:53 -0500

    Update script.js
    added places constant

--- lab4/part1/script.js
+++ lab4/part1/script.js
@@ -12,11 +12,8 @@ function randomValueFromArray(array) {
 // Text strings
 
 const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const places = ["the soup kitchen", "Disneyland", "the White House"];
 
-
-// the soup kitchen
-// Disneyland
-// the White House
 
 // spontaneously combusted
 // melted into a puddle on the sidewalk
@@ -25,6 +22,9 @@ const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 // Partial return random string function
 
 function returnRandomStoryString() {
+
+
+
   // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
 
   return storyText;
