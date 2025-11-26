a48ced24d5b4feac73f381b4799b92f8458a50c8
nolansavage <nolan.savage@dcmail.ca>
2025-11-26 16:52:20 -0500

    Update script.js

--- lab4/part1/script.js
+++ lab4/part1/script.js
@@ -22,12 +22,12 @@ const randomCharacter = randomValueFromArray(characters);
 const randomPlace = randomValueFromArray(places);
 const randomEvent = randomValueFromArray(events);
 
-let storyText = 'It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to $randomPlace}, they stared in horror for a few moments, then $randomEvent}. Bob saw the whole thing, but was not surprised — $randomCharacter weighs 300 pounds, and it was a hot day.';
+let storyText = 'It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — $randomCharacter weighs 300 pounds, and it was a hot day.';
 
   return storyText;
 }
 
-// Event listener and partial generate function definition
+// Event listener and generate function definition
 
 generateBtn.addEventListener("click", generateStory);
 
@@ -35,16 +35,16 @@ function generateStory() {
   let newStory = returnRandomStoryString();
 
 
-  if (customName.value "customName") {
+  if (customName.value !== "") {
     const name = customName.value;
+    newStory = newStory.replace("Bob", name);
   }
 
   if (document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature = Math.round(94);
+    const weight = '${Math.round(300 / 14)}stone';
+    const temperature = '${Math.round(94 - 32) 8 (5 / 9))} Celsius';
   }
 
-  // TODO: replace "" with the correct expression
-  story.textContent = "";
+  story.textContent = newStory;
   story.style.visibility = "visible";
 }
